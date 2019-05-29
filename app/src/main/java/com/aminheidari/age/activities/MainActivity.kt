190ab187016d5c06cb29f3490f4d3e2bfc24840f
Amin Heidari <dev@aminheidari.com>
2019-05-28 20:26:01 -0400

    WIP

---
+++ app/src/main/java/com/aminheidari/age/activities/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.aminheidari.age.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.aminheidari.age.R
+
+class MainActivity : AppCompatActivity() {
+
+    // ====================================================================================================
+    // region Constants/Types
+    // ====================================================================================================
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // endregion
+
+}

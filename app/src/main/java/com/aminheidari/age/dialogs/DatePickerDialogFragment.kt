d7147f422760969a30faa5356bc9914a830a8414
Amin <dev@aminheidari.com>
2019-06-07 16:38:07 -0400

    AGE-21 Added missing file.

---
+++ app/src/main/java/com/aminheidari/age/dialogs/DatePickerDialogFragment.kt
@@ -0,0 +1,27 @@
+package com.aminheidari.age.dialogs
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.aminheidari.age.R
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class DatePickerDialogFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return TextView(activity).apply {
+            setText(R.string.hello_blank_fragment)
+        }
+    }
+
+
+}

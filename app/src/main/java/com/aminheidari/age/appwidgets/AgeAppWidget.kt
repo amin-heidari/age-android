629472cbd3f1a2f3727a3078283cee23782b3ff2
Amin Heidari <dev@aminheidari.com>
2019-06-20 21:05:16 -0400

    AGE-73 Refreshing the age when the widget is tapped.

--- app/src/main/java/com/aminheidari/age/appwidgets/AgeAppWidget.kt
+++ app/src/main/java/com/aminheidari/age/appwidgets/AgeAppWidget.kt
@@ -1,8 +1,11 @@
 package com.aminheidari.age.appwidgets
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.RemoteViews
 import com.aminheidari.age.R
@@ -21,6 +24,8 @@ class AgeAppWidget : AppWidgetProvider() {
 
     companion object {
 
+        const val ACTION_REFRESH_AGE = "ACTION_REFRESH_AGE"
+
         private var calculator: AgeCalculator? = null
 
         private fun updateAppWidget(
@@ -32,10 +37,17 @@ class AgeAppWidget : AppWidgetProvider() {
 
             val calc = calculator
             if (calc != null) {
-                views.setTextViewText(R.id.appwidget_text, String.format("%10.8f", calc.currentAge.value))
+                views.setTextViewText(R.id.textView, String.format("%10.8f", calc.currentAge.value))
             } else {
-                views.setTextViewText(R.id.appwidget_text, "Set Up!")
+                views.setTextViewText(R.id.textView, "Set Up!")
             }
+
+            val intent = Intent(context, AgeAppWidget::class.java)
+            intent.setAction(ACTION_REFRESH_AGE)
+
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            views.setOnClickPendingIntent(R.id.rootLayout, pendingIntent)
 
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
@@ -107,6 +119,32 @@ class AgeAppWidget : AppWidgetProvider() {
         super.onRestored(context, oldWidgetIds, newWidgetIds)
 
         Logger.d("onRestored")
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+
+        if (context == null) { return }
+        if (intent == null) { return }
+
+        when (intent.action) {
+            ACTION_REFRESH_AGE -> {
+                val views = RemoteViews(context.packageName, R.layout.appwidget_age)
+
+                val calc = calculator
+                if (calc != null) {
+                    views.setTextViewText(R.id.textView, String.format("%10.8f", calc.currentAge.value))
+                } else {
+                    views.setTextViewText(R.id.textView, "Set Up!")
+                }
+
+                val appWidget = ComponentName(context, AgeAppWidget::class.java)
+                val appWidgetManager = AppWidgetManager.getInstance(context)
+
+                appWidgetManager.updateAppWidget(appWidget, views)
+            }
+        }
+
     }
 
     // endregion

b4fdd5a32e59145ff5bf9d57f8e54bc7428d437c
Amin <dev@aminheidari.com>
2019-08-17 23:37:25 -0400

    Added missing files.

---
+++ app/src/main/java/com/aminheidari/age/fragments/MultipleAgesPromoFragment.kt
@@ -0,0 +1,25 @@
+package com.aminheidari.age.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aminheidari.age.R
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class MultipleAgesPromoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_multiple_ages_promo, container, false)
+    }
+
+
+}

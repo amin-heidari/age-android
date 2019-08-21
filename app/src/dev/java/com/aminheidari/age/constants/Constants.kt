f76406172ea182f128ae11ef122fd7c3498aa4e9
Amin <dev@aminheidari.com>
2019-08-21 12:23:40 -0400

    Minor clean up.

--- app/src/dev/java/com/aminheidari/age/constants/Constants.kt
+++ app/src/dev/java/com/aminheidari/age/constants/Constants.kt
@@ -17,10 +17,12 @@ object Constants {
         const val apiKeyHeaderValue = "cCZA3IPS2K6R6tTzaRJrK3wVzinwogXk3A0nim8K"
 
         // Time interval (in seconds) during which the cache will be used rather than making a new api call.
+        // If this is lower than the life time of a single application process, then we'll have a re-fetch of the config on each app launch.
         const val freshCacheTime = 1
 
         // Time interval (in seconds) after which the cache expires and a fresh remote config MUST be fetched.
-        const val expireTime = 10
+        // Note that, since we don't do a re-fetch in the same app session, this must be much longer than an application process lifetime (in the order of hours if not days).
+        const val expireTime = 60 * 10
     }
 
     object AgeCalculation {

1a2396c0c9ea449cd4dd5c4ace08690e8d6a2572
Amin <dev@aminheidari.com>
2019-09-08 18:18:04 -0400

    AGE-121 Gauge view drawn successfully.
    Going to animate it next.

--- app/src/main/java/com/aminheidari/age/widgets/RingView.kt
+++ app/src/main/java/com/aminheidari/age/widgets/RingView.kt
@@ -3,12 +3,22 @@ package com.aminheidari.age.widgets
 import android.content.Context
 import android.content.res.TypedArray
 import android.graphics.Canvas
+import android.graphics.DashPathEffect
 import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import com.aminheidari.age.R
+import com.aminheidari.age.utils.dpToPx
+import com.aminheidari.age.utils.pxToDp
+import kotlin.math.PI
+import kotlin.math.min
 import kotlin.properties.Delegates
 
+/**
+ * This custom view is only to be used with a 1:1 aspect ratio.
+ * Other aspect ratios are not supported.
+ */
 class RingView : View {
 
     // ====================================================================================================
@@ -27,27 +37,26 @@ class RingView : View {
     // region API
     // ====================================================================================================
 
+    var strokeColor: Int by Delegates.observable(0, { _, _, newValue ->
+        invalidate()
+        requestLayout()
+    })
+
+    var strokeWidth: Float by Delegates.observable(0f, { _, _, newValue ->
+        invalidate()
+        requestLayout()
+    })
+
+    var segmentsCount: Int by Delegates.observable(0, { _, _, newValue ->
+        invalidate()
+        requestLayout()
+    })
+
     // endregion
 
     // ====================================================================================================
     // region Life Cycle
     // ====================================================================================================
-
-    init {
-//        context.theme.obtainStyledAttributes(
-//            attrs,
-//            R.styleable.RingView,
-//            0, 0).apply {
-//
-//            try {
-//                strokeColor2 = getColor(R.styleable.RingView_strokeColor, 0)
-////                val strokeWidthh = getDimension(R.styleable.RingView_strokeWidth, 0f)
-////                val segmentsCountd = getInt(R.styleable.RingView_segmentsCount, 0)
-//            } finally {
-//                recycle()
-//            }
-//        }
-    }
 
     constructor(context: Context) : super(context, null) {
         sharedInit()
@@ -67,20 +76,37 @@ class RingView : View {
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-    }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        // Paddings
+        ovalRect = RectF(
+            paddingLeft.toFloat(),
+            paddingTop.toFloat(),
+            (width - paddingRight).toFloat(),
+            (height- paddingBottom).toFloat()
+        )
+
+        // Stroke width inset.
+        ovalRect.inset(strokeWidth / 2f, strokeWidth / 2f)
+
+        // Paint.
+        strokePaint.color = strokeColor
+        strokePaint.strokeWidth = strokeWidth
+
+        if (segmentsCount > 1) {
+            val ellipseRadius = min(ovalRect.width(), ovalRect.height())
+            val ellpiseCircumference = 2f * PI * ellipseRadius
+            val onOffInterval = ellpiseCircumference.toFloat() / (2f * segmentsCount.toFloat()) / 2f
+            strokePaint.pathEffect = DashPathEffect(floatArrayOf(onOffInterval, onOffInterval), 0f)
+        } else {
+            strokePaint.pathEffect = null
+        }
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
         canvas?.apply {
-            strokePaint.color = strokeColor//resources.getColor(R.color.accent)
-            strokePaint.strokeWidth = strokeWidth
-
-            drawOval(0f, 0f, 200f, 200f, strokePaint)
+            drawOval(ovalRect, strokePaint)
         }
     }
 
@@ -92,24 +118,10 @@ class RingView : View {
 
     private val strokePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
+        strokeCap = Paint.Cap.ROUND
     }
 
-    private var strokeColor2: Int = 174
-
-    private var strokeColor: Int by Delegates.observable(0, { _, _, newValue ->
-        invalidate()
-        requestLayout()
-    })
-
-    private var strokeWidth: Float by Delegates.observable(0f, { _, _, newValue ->
-        invalidate()
-        requestLayout()
-    })
-
-    private var segmentsCount: Int by Delegates.observable(0, { _, _, newValue ->
-        invalidate()
-        requestLayout()
-    })
+    private var ovalRect: RectF = RectF(0f, 0f, 0f, 0f)
 
     // endregion
 

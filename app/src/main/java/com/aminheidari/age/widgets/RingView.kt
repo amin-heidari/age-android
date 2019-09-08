8d6ce00585ff8839ec3b79fc874e5fc95d873de3
Amin <dev@aminheidari.com>
2019-09-08 16:14:19 -0400

    AGE-121 WIP

--- app/src/main/java/com/aminheidari/age/widgets/RingView.kt
+++ app/src/main/java/com/aminheidari/age/widgets/RingView.kt
@@ -1,6 +1,7 @@
 package com.aminheidari.age.widgets
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
@@ -8,7 +9,7 @@ import android.view.View
 import com.aminheidari.age.R
 import kotlin.properties.Delegates
 
-class RingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+class RingView : View {
 
     // ====================================================================================================
     // region Static
@@ -33,18 +34,34 @@ class RingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     // ====================================================================================================
 
     init {
-        context.theme.obtainStyledAttributes(
-            attrs,
-            R.styleable.RingView,
-            0, 0).apply {
+//        context.theme.obtainStyledAttributes(
+//            attrs,
+//            R.styleable.RingView,
+//            0, 0).apply {
+//
+//            try {
+//                strokeColor2 = getColor(R.styleable.RingView_strokeColor, 0)
+////                val strokeWidthh = getDimension(R.styleable.RingView_strokeWidth, 0f)
+////                val segmentsCountd = getInt(R.styleable.RingView_segmentsCount, 0)
+//            } finally {
+//                recycle()
+//            }
+//        }
+    }
 
-            try {
-                strokeColor = getColor(R.styleable.RingView_strokeColor, 0)
-                strokeWidth = getDimension(R.styleable.RingView_strokeColor, 0f)
-                segmentsCount = getColor(R.styleable.RingView_strokeColor, 0)
-            } finally {
-                recycle()
-            }
+    constructor(context: Context) : super(context, null) {
+        sharedInit()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs, 0) {
+        attrs?.let { attrSet ->
+            sharedInit(context.theme.obtainStyledAttributes(attrSet, R.styleable.RingView, 0, 0))
+        }
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        attrs?.let { attrSet ->
+            sharedInit(context.theme.obtainStyledAttributes(attrSet, R.styleable.RingView, defStyleAttr, 0))
         }
     }
 
@@ -59,9 +76,11 @@ class RingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        canvas?.let { c ->
-            strokePaint.color = strokeColor
+        canvas?.apply {
+            strokePaint.color = strokeColor//resources.getColor(R.color.accent)
             strokePaint.strokeWidth = strokeWidth
+
+            drawOval(0f, 0f, 200f, 200f, strokePaint)
         }
     }
 
@@ -74,6 +93,8 @@ class RingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private val strokePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
     }
+
+    private var strokeColor2: Int = 174
 
     private var strokeColor: Int by Delegates.observable(0, { _, _, newValue ->
         invalidate()
@@ -95,6 +116,14 @@ class RingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     // ====================================================================================================
     // region Methods
     // ====================================================================================================
+
+    private fun sharedInit(typedArray: TypedArray? = null) {
+        typedArray?.apply {
+            strokeColor = getColor(R.styleable.RingView_strokeColor, 0)
+            strokeWidth = getDimension(R.styleable.RingView_strokeWidth, 0f)
+            segmentsCount = getInt(R.styleable.RingView_segmentsCount, 0)
+        }
+    }
 
     // endregion
 
